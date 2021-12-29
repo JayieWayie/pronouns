103882579cf59aa9defad300462cbcbf7c5e6f41
Jay <jeffreybennett@live.co.uk>
2021-12-29 07:39:24 +0000

    Started Development.
    Development began of pronouns plugin.

---
+++ src/main/java/me/jayie/pronouns/Pronouns.java
@@ -0,0 +1,36 @@
+package me.jayie.pronouns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pronouns extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+
+    public void commands(){
+        // Execute all command classes.
+
+    }
+
+
+
+    public void listeners(){
+        // Execute all listener classes.
+
+    }
+
+
+    public void startupMessage(){
+        // Startup Message
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
